be6998052082a0762e6e3a3ba63c9fe16d2509e9
zpopkinh <80115153+zpopkinh@users.noreply.github.com>
2025-02-04 14:58:23 -0500

    Update Pm_HC_genotype_gVCFs.sh
    Added variantstotable code to the end

--- Pm_bash_scripts/Pm_HC_genotype_gVCFs.sh
+++ Pm_bash_scripts/Pm_HC_genotype_gVCFs.sh
@@ -22,3 +22,5 @@ cd Picard_output
 gatk GenomicsDBImport --sample-name-map ../Pm.sample_map --genomicsdb-workspace-path ../Variants/gVCF -L /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta.bed
 
 gatk GenotypeGVCFs -R /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta -V gendb://../Variants/gVCF -O ../Variants/Pm_HC_raw.vcf.gz
+
+gatk VariantsToTable -V Pm_HC_raw.vcf.gz -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -O raw.table
