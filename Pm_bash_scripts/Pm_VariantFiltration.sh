9b2e3f95069adb6ffdabf174b99e7f2e5f900a11
zpopkinh <80115153+zpopkinh@users.noreply.github.com>
2025-02-04 15:00:19 -0500

    Create Pm_VariantFiltration.sh

---
+++ Pm_bash_scripts/Pm_VariantFiltration.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+#SBATCH -p general
+#SBATCH -N 1
+#SBATCH --mem=100g
+#SBATCH -t 8:00:00
+
+module add gatk
+
+module add samtools
+
+cd /work/users/z/p/zpopkinh/Pm_rerun/Variants/
+
+	gatk VariantFiltration \
+	-R /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta/ \
+	-V Pm_HC_raw.vcf.gz \
+	-O Pm_HC_hard_filtered.vcf.gz \
+	--filter-name "lowQD" \
+	--filter-expression "QD<2.5" \
+	--filter-name "highFS" \
+	--filter-expression "FS>10.0" \
+	--filter-name "lowMQ" \
+	--filter-expression "MQ<50.0" \
+	--filter-name "lowMQRankSum" \
+	--filter-expression "MQRankSum<-2.5" \
+	--filter-name "lowReadPosRankSum" \
+	--filter-expression "ReadPosRankSum<-2.5"
+
+bcftools view -m2 -M2 -v snps Pm_HC_hard_filtered.vcf.gz -O z -o Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz
+
+gatk VariantsToTable -V Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -O filtered_biallelics.table
+
