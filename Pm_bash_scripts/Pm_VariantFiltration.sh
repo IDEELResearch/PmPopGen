8c042d4848b30a8aac671120a3f6fcb03a0dd52e
zpopkinh <80115153+zpopkinh@users.noreply.github.com>
2025-05-09 15:58:19 -0400

    Update Pm_VariantFiltration.sh
    Fixed a mistake in the bcftools commands

--- Pm_bash_scripts/Pm_VariantFiltration.sh
+++ Pm_bash_scripts/Pm_VariantFiltration.sh
@@ -32,8 +32,8 @@ bcftools view -m2 -M2 -v snps Pm_HC_hard_filtered.vcf.gz -O z -o Pm_HC_hard_filt
 
 gatk VariantsToTable -V Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -O filtered_biallelics.table
 
-bcftools view -R ^Pmalariae_trf.bed -Oz -o Pm_TRs_masked.vcf.gz Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz
+bcftools view -T ^Pmalariae_trf_sort.bed -Oz -o Pm_TRs_masked.vcf.gz Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz
 
-bcftools view -R ^Pm_PIRs.bed -Oz -o Pm_PIRs_masked.vcf.gz Pm_TRs_masked.vcf.gz
+bcftools view -T ^Pm_PIRs.bed -Oz -o Pm_PIRs_masked.vcf.gz Pm_TRs_masked.vcf.gz
 
 vcftools --gzvcf Pm_PIRs_masked.vcf.gz --max-missing 0.8 --recode --recode-INFO-all --stdout | gzip -c > Pm_HC_missingness_filtered_first.vcf.gz
