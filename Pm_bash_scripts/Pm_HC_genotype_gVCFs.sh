76d90789a2728528a17f96a3ed7225be82ecca19
kcareyewend <42275420+kcareyewend@users.noreply.github.com>
2026-02-01 10:40:45 -0500

    Update Pm_HC_genotype_gVCFs.sh

--- Pm_bash_scripts/Pm_HC_genotype_gVCFs.sh
+++ Pm_bash_scripts/Pm_HC_genotype_gVCFs.sh
@@ -1,3 +1,9 @@
+###############################################################
+############### Pm_HC_gentype_gVCFs ###########################
+###############################################################
+#Description: uses GATK GenotypeGVCFs to genotype individual gVCF 
+#files across entire sample pool, yielding vcf file showing variant sites
+
 #!/bin/bash
 ##############################################################
 
@@ -19,8 +25,11 @@ mkdir Variants
 
 cd Picard_output
 
+#generate DBImport database of samples, reference genome intervals, and output directory
 gatk GenomicsDBImport --sample-name-map ../Pm.sample_map --genomicsdb-workspace-path ../Variants/gVCF -L /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta.bed
 
+#produce VCF file containing all variants in Pm genome across all samples
 gatk GenotypeGVCFs -R /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta -V gendb://../Variants/gVCF -O ../Variants/Pm_HC_raw.vcf.gz
 
+#output table of variant position, statistics, and quality metrics
 gatk VariantsToTable -V Pm_HC_raw.vcf.gz -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -O raw.table
