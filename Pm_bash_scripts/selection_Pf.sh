bb417cc35e5b9533a993250b36d3696337e949a1
kcareyewend <42275420+kcareyewend@users.noreply.github.com>
2026-02-01 12:02:22 -0500

    Update selection_Pf.sh

--- Pm_bash_scripts/selection_Pf.sh
+++ Pm_bash_scripts/selection_Pf.sh
@@ -1,3 +1,10 @@
+###############################################################
+###################### selection_Pf ############################
+###############################################################
+#Description: generates bed file showing genomic windows containing
+#specific genes of interest (ama1, crt, csp, dhfr-ts, Kelch13, 
+#lsa1, mdr1, mdr2, mrp1, mrp2, msp1, p25, p48-45, pppk-dhps, trap)
+
 #!/bin/bash
 ##############################################################
 
