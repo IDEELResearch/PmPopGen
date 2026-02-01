315f8326269fa01b9634ccf6535dcb97b9c931e7
kcareyewend <42275420+kcareyewend@users.noreply.github.com>
2026-02-01 11:02:27 -0500

    Update Pm_VariantFiltration.sh

--- Pm_bash_scripts/Pm_VariantFiltration.sh
+++ Pm_bash_scripts/Pm_VariantFiltration.sh
@@ -1,3 +1,10 @@
+###############################################################
+################# Pm_VariantFiltration ########################
+###############################################################
+#Description: applies hard quality filtering and missingness thresholds to VCF file,
+#limits to SNPs, and excludes hypervariable regions
+
+
 #! /bin/bash
 
 #SBATCH -p general
@@ -13,10 +20,12 @@ module add vcftools
 
 cd /work/users/z/p/zpopkinh/Pm_rerun/Variants/
 
+#apply quality filter threshoolds
 	gatk VariantFiltration \
 	-R /proj/ideel/resources/genomes/Pmalariae/PlasmoDB-67_PmalariaeUG01_Genome.fasta/ \
 	-V Pm_HC_raw.vcf.gz \
 	-O Pm_HC_hard_filtered.vcf.gz \
+	#each filter expression must be updated with final threshold
 	--filter-name "lowQD" \
 	--filter-expression "QD<2.5" \
 	--filter-name "highFS" \
@@ -28,12 +37,17 @@ cd /work/users/z/p/zpopkinh/Pm_rerun/Variants/
 	--filter-name "lowReadPosRankSum" \
 	--filter-expression "ReadPosRankSum<-2.5"
 
+#limit variants to SNPs only
 bcftools view -m2 -M2 -v snps Pm_HC_hard_filtered.vcf.gz -O z -o Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz
 
+#output table of SNPs
 gatk VariantsToTable -V Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz -F QD -F FS -F MQ -F MQRankSum -F ReadPosRankSum -O filtered_biallelics.table
 
+#mask tandem repeats
 bcftools view -T ^Pmalariae_trf_sort.bed -Oz -o Pm_TRs_masked.vcf.gz Pm_HC_hard_filtered_biallelic_snps_only.vcf.gz
 
+#mask hypervariable Plasmodium Interspersed Repeat (pir) regions
 bcftools view -T ^Pm_PIRs.bed -Oz -o Pm_PIRs_masked.vcf.gz Pm_TRs_masked.vcf.gz
 
+#excludes variant sites with missing data in >20% of samples
 vcftools --gzvcf Pm_PIRs_masked.vcf.gz --max-missing 0.8 --recode --recode-INFO-all --stdout | gzip -c > Pm_HC_missingness_filtered_first.vcf.gz
